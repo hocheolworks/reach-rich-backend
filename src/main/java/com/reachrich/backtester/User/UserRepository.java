458a797c53627955d6cfd2d438905fc15e6e6c40
JeongCheolLee <jclee7503@gmail.com>
2024-09-05 17:30:16 +0900

    코틀린으로 변환 전 전체 삭제

--- src/main/java/com/reachrich/backtester/User/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.reachrich.backtester.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/main/java/com/reachrich/backtester/User/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.reachrich.backtester.User;
-
-public class UserService {
-}
