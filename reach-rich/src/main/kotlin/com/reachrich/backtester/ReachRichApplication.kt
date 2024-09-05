4f22f7e62f9ff75480e089dc16739d360a4ba7e6
JeongCheolLee <jclee7503@gmail.com>
2024-09-05 17:34:54 +0900

    kopring initial commit

---
+++ reach-rich/src/main/kotlin/com/reachrich/backtester/ReachRichApplication.kt
@@ -0,0 +1,11 @@
+package com.reachrich.backtester
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReachRichApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReachRichApplication>(*args)
+}
