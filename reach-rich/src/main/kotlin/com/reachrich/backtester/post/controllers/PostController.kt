ff6b41d8bf68abd4074ed9774895c2bba932c140
JeongCheolLee <jclee7503@gmail.com>
2024-09-06 14:56:18 +0900

    ing: 샘플 CRUD 진행 중(게시글)

--- reach-rich/src/main/kotlin/com/reachrich/backtester/post/controllers/PostController.kt
+++ reach-rich/src/main/kotlin/com/reachrich/backtester/post/controllers/PostController.kt
@@ -2,37 +2,42 @@ package com.reachrich.backtester.post.controllers
 
 import com.reachrich.backtester.post.entities.Post
 import com.reachrich.backtester.post.services.PostService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/posts")
 class PostController(private val postService: PostService) {
 
+    @Operation(summary = "게시글 목록 조회", description = "모든 게시글의 목록을 조회합니다.")
     @GetMapping
-    fun getAllPosts(): List<Post> {
-        return postService.getAllPosts()
+    fun findAllPosts(): List<Post> {
+        return postService.findAllPosts()
     }
 
+
+    @Operation(summary = "게시글 단건 조회", description = "ID로 특정 게시글을 조회합니다.")
     @GetMapping("/{id}")
     fun getPostById(@PathVariable id: Long): Post {
-        return postService.getPostById(id)
+        return postService.findPostById(id)
     }
 
+    @Operation(summary = "게시글 작성", description = "새로운 게시글을 작성합니다.")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun createPost(@RequestBody post: Post): Post {
-        return postService.createPost(post)
+        return postService.generatePost(post)
     }
 
     @PutMapping("/{id}")
     fun updatePost(@PathVariable id: Long, @RequestBody post: Post): Post {
-        return postService.updatePost(id, post)
+        return postService.modifyPost(id, post)
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deletePost(@PathVariable id: Long) {
-        postService.deletePost(id)
+        postService.removePost(id)
     }
 }
--- reach-rich/src/main/kotlin/com/reachrich/backtester/post/services/PostService.kt
+++ reach-rich/src/main/kotlin/com/reachrich/backtester/post/services/PostService.kt
@@ -8,32 +8,33 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class PostService(private val postRepository: PostRepository) {
 
-    fun getAllPosts(): List<Post> {
+    fun findAllPosts(): List<Post> {
         return postRepository.findAll()
     }
 
-    fun getPostById(id: Long): Post {
+    fun findPostById(id: Long): Post {
         return postRepository.findById(id).orElseThrow { IllegalArgumentException("com.reachrich.backtester.post.entities.Post not found: $id") }
     }
 
     @Transactional
-    fun createPost(post: Post): Post {
+    fun generatePost(post: Post): Post {
         return postRepository.save(post)
     }
 
     @Transactional
-    fun updatePost(id: Long, updatedPost: Post): Post {
-        val existingPost = getPostById(id)
+    fun modifyPost(id: Long, updatedPost: Post): Post {
+        val existingPost = findPostById(id)
         val postToSave = existingPost.copy(
             title = updatedPost.title,
             content = updatedPost.content,
             updatedAt = updatedPost.updatedAt
         )
+
         return postRepository.save(postToSave)
     }
 
     @Transactional
-    fun deletePost(id: Long) {
+    fun removePost(id: Long) {
         postRepository.deleteById(id)
     }
 }
